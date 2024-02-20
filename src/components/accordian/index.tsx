240c3bfd383431fc1b6905998dba690ebff8b673
vtouzard <vtouzard244@gmail.com>
2024-02-20 18:19:31 +0100

    feat: Ajout de la logique pour le composant accordian

--- src/components/accordian/index.tsx
+++ src/components/accordian/index.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from "react";
+import data from "./data";
 
 const Acordian = () => {
-  return (
-    <div>Acordian</div>
-  )
-}
+  const [selected, setSelected] = useState<string>();
 
-export default Acordian
+  const handleSingleSelection = (id: string, e: React.MouseEvent) => {
+    setSelected(id === selected ? '' : id);
+  };
+
+  return (
+    <div className="wrapper">
+      <div className="accordian">
+        {data && data.length > 0 ? (
+          data.map((dataItem) => (
+            <div className="item" key={dataItem.id}>
+              <div
+                onClick={(e) => handleSingleSelection(dataItem.id, e)}
+                className="title"
+              >
+                <h3>{dataItem.question}</h3>
+                <span>+</span>
+              </div>
+              {selected === dataItem.id ? (
+                <div className="content">{dataItem.answer}</div>
+              ) : null}
+            </div>
+          ))
+        ) : (
+          <div>No data Found</div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Acordian;
