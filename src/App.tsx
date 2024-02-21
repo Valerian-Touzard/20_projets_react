a3dcddc10517726a8aa5365008448fe491f2b643
vtouzard <vtouzard244@gmail.com>
2024-02-21 01:14:58 +0100

    feat: Ajout du composant RandomColor

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import Acordian from './components/accordian';
+import RandomColor from './components/random-color';
 
 function App() {
   return (
     <div className="App">
       {/* Accordian component */}
       <Acordian />
+
+      {/* Random Color Component */}
+      <RandomColor />
     </div>
   );
 }
