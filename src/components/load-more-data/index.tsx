ec63a76013d9b3962d6f31cf69933abf492eaa61
vtouzard <vtouzard244@gmail.com>
2024-02-22 11:36:23 +0100

    feat: Ajout de la logique de récupération et d'affichages des images pour le composant LoadMoreData

--- src/components/load-more-data/index.tsx
+++ src/components/load-more-data/index.tsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+
+type FetchData = {
+  products: Product[];
+  total: number;
+  skip: number;
+  limit: number;
+};
+
+type Product = {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  brand: string;
+  category: string;
+  thumbnail: string;
+  images: string[];
+};
 
 const LoadMoreData = () => {
-  return (
-    <div>LoadMoreData</div>
-  )
-}
+  const [loading, setLoading] = useState(false);
+  const [products, setProducts] = useState<Product[]>([]);
+  const [count, setCount] = useState(0);
 
-export default LoadMoreData
+  useEffect(() => {
+    fetchProduct();
+  }, []);
+
+  const fetchProduct = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(
+        `https://dummyjson.com/products?limit=10&skip=${
+          count === 0 ? 0 : count * 20
+        }`
+      );
+
+      const result = await response.json();
+
+      if (result && result.products && result.products.length) {
+        setProducts(result.products);
+        setLoading(false);
+      }
+
+      console.log(result);
+    } catch (e) {
+      console.error(e);
+      setLoading(false);
+    }
+  };
+
+  if (loading) {
+    return <div>Loading data ! Please wait</div>;
+  }
+
+  return (
+    <div className="container">
+      <div className="product-container">
+        {products && products.length
+          ? products.map((product) => <div key={product.id} className="product">
+            <img src={product.thumbnail} alt={product.title} />
+            <p>{product.title}</p>
+          </div>)
+          : null}
+      </div>
+      <div className="button-container">
+        <button>Load more products</button>
+      </div>
+    </div>
+  );
+};
+
+export default LoadMoreData;
