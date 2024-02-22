0bcbdfcadf3c9839763e9b1654bed31a976b574e
vtouzard <vtouzard244@gmail.com>
2024-02-22 09:57:19 +0100

    feat: Ajout de la structure pour notre composant image-slider

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import Acordian from './components/accordian';
 import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
+import ImageSlider from './components/image-slider';
 
 function App() {
   return (
@@ -15,6 +16,9 @@ function App() {
 
       {/* Star Rating Component */}
       <StarRating noOfStars={10} />
+
+      {/* Image Slider */}
+      <ImageSlider />
     </div>
   );
 }
---
+++ src/components/image-slider/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+type Props = {
+    url?: string;
+    limit?: number;
+}
+
+const ImageSlider = ({url, limit}: Props) => {
+  return (
+    <div>ImageSlider</div>
+  )
+}
+
+export default ImageSlider
