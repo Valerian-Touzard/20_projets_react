2f9c789f22160cd55527dca9a477d6c4ba6979da
vtouzard <vtouzard244@gmail.com>
2024-02-23 10:41:47 +0100

    feat: Ajout de la logique pour afficher/cacher les différends menus

--- src/components/tree-view/menu-item.tsx
+++ src/components/tree-view/menu-item.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Menu } from "./data";
 import MenuList from "./menu-list";
 
@@ -6,12 +6,40 @@ type Props = {
   item: Menu;
 };
 
+type DisplayCurrentChildren = {
+  [key: string]: boolean;
+};
+
 const MenuItem = ({ item }: Props) => {
+  const [displayCurrentChildren, setDisplayCurrentChildren] =
+    useState<DisplayCurrentChildren>({});
+
+  const handleToggleChildren = (label: string) => {
+    setDisplayCurrentChildren({
+      ...displayCurrentChildren,
+      [label]: !displayCurrentChildren[label],
+    });
+  };
+
+  console.log(displayCurrentChildren);
+
   return (
     <li>
-      <p>{item.label}</p>
+      <div style={{ display: "flex", gap: "20px" }}>
+        <p>{item.label}</p>
+        {item && item.children && item.children.length ? (
+          <span onClick={() => handleToggleChildren(item.label)}>
+            {
+                displayCurrentChildren[item.label] ? '-' : '+'
+            }
+          </span>
+        ) : null}
+      </div>
 
-      {item && item.children && item.children.length > 0 ? (
+      {item &&
+      item.children &&
+      item.children.length > 0 &&
+      displayCurrentChildren[item.label] ? (
         <MenuList list={item.children} />
       ) : null}
     </li>
