cc905654d7af52a1590cfdfb425e8ed5e1c1f079
vtouzard <vtouzard244@gmail.com>
2024-02-23 11:58:07 +0100

    feat: Ajout de la logique pour générer un qrCode

--- src/components/qr-code-generator/index.tsx
+++ src/components/qr-code-generator/index.tsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import QRCode from "react-qr-code";
 
 const QrCodeGenerator = () => {
-  return <div>QrCodeGenerator</div>;
+  const [qrCode, setQrCode] = useState("");
+  const [input, setInput] = useState("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  };
+
+  const handleGenerate = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setQrCode(input as string);
+    setInput("");
+  };
+
+  return (
+    <div>
+      <h1>QR Code Generator</h1>
+      <div>
+        <input
+          onChange={handleChange}
+          type="text"
+          name="qr-code"
+          placeholder="Enter your value here"
+        />
+        <button
+          disabled={input && input.trim() !== "" ? false : true}
+          onClick={handleGenerate}
+        >
+          Generate
+        </button>
+      </div>
+      <div>
+        <QRCode id="qr-code-value" value={qrCode} size={400} bgColor="#fff" />
+      </div>
+    </div>
+  );
 };
 
 export default QrCodeGenerator;
