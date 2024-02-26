f53f80fe720edf9640b1f47808a49a758f18ebff
vtouzard <vtouzard244@gmail.com>
2024-02-26 13:11:43 +0100

    style: Indentation du code

--- src/components/star-rating/index.tsx
+++ src/components/star-rating/index.tsx
@@ -1,44 +1,42 @@
 import React, { useState } from "react";
 import { FaStar } from "react-icons/fa";
-import './style.css'
+import "./style.css";
 
 type StarRatingProps = {
   noOfStars: number;
 };
 
-const StarRating = ({
-  noOfStars,
-}: StarRatingProps) => {
+const StarRating = ({ noOfStars }: StarRatingProps) => {
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
 
   const handleClick = (currentIndex: number, e?: React.MouseEvent) => {
-    setRating(currentIndex)
+    setRating(currentIndex);
   };
   const handleMouseEnter = (currentIndex: number, e?: React.MouseEvent) => {
-    setHover(currentIndex)
+    setHover(currentIndex);
   };
   const handleMouseLeave = (e?: React.MouseEvent) => {
-    setHover(rating)
+    setHover(rating);
   };
-
-
 
   return (
     <div className="star-rating">
-      {Array(noOfStars).fill(null).map((_, index) => {
-        index += 1;
-        return (
-          <FaStar
-            key={index}
-            className={index <= (hover || rating) ? 'active' : 'inactive'}
-            onClick={() => handleClick(index)}
-            onMouseEnter={() => handleMouseEnter(index)}
-            onMouseLeave={() => handleMouseLeave()}
-            size={40}
-          />
-        );
-      })}
+      {Array(noOfStars)
+        .fill(null)
+        .map((_, index) => {
+          index += 1;
+          return (
+            <FaStar
+              key={index}
+              className={index <= (hover || rating) ? "active" : "inactive"}
+              onClick={() => handleClick(index)}
+              onMouseEnter={() => handleMouseEnter(index)}
+              onMouseLeave={() => handleMouseLeave()}
+              size={40}
+            />
+          );
+        })}
     </div>
   );
 };
