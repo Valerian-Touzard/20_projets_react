184a069e4043c0dbac6c31c1e1df57d70da73199
vtouzard <vtouzard244@gmail.com>
2024-02-27 11:27:42 +0100

    feat: Ajout du JSX pour la modal

--- src/components/custom-modal-popup/modal.tsx
+++ src/components/custom-modal-popup/modal.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
 
-const Modal = () => {
+type Props = {
+  id?: string;
+  header?: string;
+  body?: string;
+  footer?: string;
+};
+
+const Modal = ({ id, header, body, footer }: Props) => {
   return (
-    <div>Modal</div>
-  )
-}
+    <div id={id || "Modal"} className="modal">
+      <div className="modal-content">
+        <div className="header">
+          <span className="close-modal-icon">&times;</span>
+          <h2>{header ? header : "Header"}</h2>
+        </div>
+        <div className="body">
+          {body ? (
+            body
+          ) : (
+            <div>
+              <p>This is our Modal Body</p>
+            </div>
+          )}
+        </div>
+        <div className="footer">{footer ? footer : <h2>Footer</h2>}</div>
+      </div>
+    </div>
+  );
+};
 
-export default Modal
+export default Modal;
