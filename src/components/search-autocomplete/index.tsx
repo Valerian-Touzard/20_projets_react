aa3143e2454450be3c30ceeda72b8676c39a4d8a
vtouzard <vtouzard244@gmail.com>
2024-02-27 16:17:29 +0100

    feat: Ajout de la récupération des data depuis l'API json

--- src/components/search-autocomplete/index.tsx
+++ src/components/search-autocomplete/index.tsx
@@ -1,6 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const SearchAutoComplete = () => {
+  const [loading, setLoading] = useState(false);
+  const [users, setUsers] = useState([]);
+  const [error, setError] = useState<Error>();
+
+  useEffect(() => {
+    fetchListOfUsers();
+  }, []);
+
+  const fetchListOfUsers = async () => {
+    try {
+      const res = await fetch("https://dummyjson.com/users");
+      const data = await res.json();
+
+      if (data && data.users && data.users.length) {
+        setUsers(data);
+        setLoading(false);
+      }
+    } catch (error) {
+      setLoading(false);
+      console.error(error as Error);
+      setError(error as Error);
+    }
+  };
+
+  if (error) {
+    return <div>An error ocured: {error.message}</div>;
+  }
+
   return (
     <div className="search-autocompete-container">
       <input
