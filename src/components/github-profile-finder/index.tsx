3bb395664e585860438e7cbfb2502e470fbaab6e
vtouzard <vtouzard244@gmail.com>
2024-02-27 12:19:24 +0100

    feat: Ajout de la récupération des informations des données des profiles utilisateur de github

--- src/components/github-profile-finder/index.tsx
+++ src/components/github-profile-finder/index.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+
 
 const GithubProfileFinder = () => {
-  return (
-    <div>GithubProfileFinder</div>
-  )
-}
+  const [username, setUsername] = useState("vtouzard");
+  const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-export default GithubProfileFinder
+  useEffect(() => {
+    fetchGithubUserData();
+  }, []);
+
+  /**
+   * Permet de récuperer les informations des profiles des utilisateurs de github
+   */
+  const fetchGithubUserData = async () => {
+    const res = await fetch(`https://api.github.com/users/${username}`);
+
+    const data = await res.json();
+    if(data) {
+        setUserData(data);
+        setLoading(false);
+    }
+  };
+
+  const handleSubmit = () => {};
+
+
+  if(loading) {
+        return <h1>Loading Data! Please wait</h1>
+  }
+  
+  return (
+    <div className="github-profile-container">
+      <div className="input-wrapper">
+        <input
+          type="text"
+          name="search-by-username"
+          placeholder="Search Github Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <button onClick={handleSubmit}>Search</button>
+      </div>
+    </div>
+  );
+};
+
+export default GithubProfileFinder;
