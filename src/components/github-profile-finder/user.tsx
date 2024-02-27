7bb3949548b60a120b8b596827190f9461aa886d
vtouzard <vtouzard244@gmail.com>
2024-02-27 15:55:11 +0100

    feat: Ajout de l'affichages des autres éléments

--- src/components/github-profile-finder/user.tsx
+++ src/components/github-profile-finder/user.tsx
@@ -24,6 +24,20 @@ const User = ({ user }: Props) => {
           })} ${createdDate.getFullYear()}`}
         </p>
       </div>
+      <div>
+        <div>
+          <p>Public Repos</p>
+          <p>{user.public_repos}</p>
+        </div>
+        <div>
+          <p>Followers</p>
+          <p>{user.followers}</p>
+        </div>
+        <div>
+          <p>Following</p>
+          <p>{user.following}</p>
+        </div>
+      </div>
     </div>
   );
 };
