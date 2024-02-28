437a913e1a58fb0ea5c6707561edf2893d09e962
vtouzard <vtouzard244@gmail.com>
2024-02-28 16:09:28 +0100

    feat: Ajout de la récupération des prénoms depuis l'API

--- src/components/search-autocomplete/index.tsx
+++ src/components/search-autocomplete/index.tsx
@@ -1,21 +1,30 @@
 import React, { useEffect, useState } from "react";
 
+type User = {
+    firstName: string;
+}
+
 const SearchAutoComplete = () => {
   const [loading, setLoading] = useState(false);
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState<Error>();
 
   useEffect(() => {
     fetchListOfUsers();
   }, []);
 
+  /**
+   * Permet de récupérer les prénoms, données depuis l'API dummyJSOn
+   */
   const fetchListOfUsers = async () => {
     try {
+      // Appel del'API
       const res = await fetch("https://dummyjson.com/users");
       const data = await res.json();
 
+      // On vérifie qi on obtien bien des données
       if (data && data.users && data.users.length) {
-        setUsers(data);
+        setUsers(data.users.map((user: User) => user.firstName ));
         setLoading(false);
       }
     } catch (error) {
