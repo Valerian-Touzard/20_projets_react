6cb4161dc71beecc031743f87339c533b186ce92
vtouzard <vtouzard244@gmail.com>
2024-03-01 01:43:23 +0100

    feat: Ajout de la logique d'affichage des X et O

--- src/components/tic-tac-toe/index.tsx
+++ src/components/tic-tac-toe/index.tsx
@@ -1,33 +1,59 @@
-import React from "react";
-import './style.css'
+import React, { useState } from "react";
+import "./style.css";
 
 type Props = {
-  value?: string
-}
+  value: string;
+  onClick: () => void;
+};
 
-const Square = ({value}: Props) => {
-return (
-  <button className='square'>{value}</button>
-)
-}
+const Square = ({ value, onClick }: Props) => {
+  const squareClick = () => {
+    onClick();
+  };
+
+  return (
+    <button onClick={onClick} className="square">
+      {value}
+    </button>
+  );
+};
 
 const TicTacToe = () => {
+  const [squares, setSquares] = useState(Array(9).fill(""));
+  const [isXTurn, setIsXTurn] = useState(true);
+
+  /**
+   * Méthode qui permet d'afficher un X ou un O lors du clique de l'utilisateur dans le carrée
+   * Elle vérifie également si le carré n'est pas déjas remplis (auquel cas, la fonction s'arrête)
+   * @param index number: L'index de la liste que l'on va vérifier
+   * @returns unknown
+   */
+  const handleClick = (index: number) => {
+    let cpySquares = [...squares];
+    if (cpySquares[index]) return;
+    cpySquares[index] = isXTurn ? "X" : "O";
+    setIsXTurn(!isXTurn);
+    setSquares(cpySquares);
+  };
+
+  
+
   return (
     <div className="tic-tac-toe-container">
       <div className="row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onClick={() => handleClick(0)} />
+        <Square value={squares[1]} onClick={() => handleClick(1)} />
+        <Square value={squares[2]} onClick={() => handleClick(2)} />
       </div>
       <div className="row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} onClick={() => handleClick(3)} />
+        <Square value={squares[4]} onClick={() => handleClick(4)} />
+        <Square value={squares[5]} onClick={() => handleClick(5)} />
       </div>
       <div className="row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} onClick={() => handleClick(6)} />
+        <Square value={squares[7]} onClick={() => handleClick(7)} />
+        <Square value={squares[8]} onClick={() => handleClick(8)} />
       </div>
     </div>
   );
