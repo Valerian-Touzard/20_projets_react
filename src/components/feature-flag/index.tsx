ac04cd4ce3406e5fc8b53ba55a936be2cf945a30
vtouzard <vtouzard244@gmail.com>
2024-03-04 11:12:31 +0100

    feat: Ajout de la liste des composant au contexte

--- src/components/feature-flag/index.tsx
+++ src/components/feature-flag/index.tsx
@@ -1,6 +1,33 @@
 import React from 'react'
+import LightDarkMode from '../light-dark-mode'
+import TicTacToe from '../tic-tac-toe'
+import RandomColor from '../random-color'
+import Acordian from '../accordian'
+import TreeView from '../tree-view'
 
 const FeatureFlags = () => {
+    const componentsToRender = [
+        {
+            key: 'showLightDarkMode',
+            component: <LightDarkMode />
+        },
+        {
+            key: 'showTicTacToeBoard',
+            component: <TicTacToe />
+        },
+        {
+            key: 'showRandomColorGenerator',
+            component: <RandomColor />
+        },
+        {
+            key: 'showAccordian',
+            component: <Acordian />
+        },
+        {
+            key: 'showTreeView',
+            component: <TreeView />
+        }
+    ]
   return (
     <div>
         <h1>Feature Flags</h1>
