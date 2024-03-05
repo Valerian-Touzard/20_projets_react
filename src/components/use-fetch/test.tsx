e4c2ae8bf0d6bb675eda69c61d880dccc77d15ed
vtouzard <vtouzard244@gmail.com>
2024-03-05 01:11:14 +0100

    feat: ajout de l'affichage de l'erreure si il y en a une

--- src/components/use-fetch/test.tsx
+++ src/components/use-fetch/test.tsx
@@ -38,6 +38,7 @@ const UseFetchHookTest = () => {
     <div>
       <h1>Use Fetch Hook</h1>
       {pending ? <h3>Pending ! Please wait</h3> : null}
+      {error ? <h3>An error occured</h3> : null}
       {data && data.products && data.products.length
         ? data.products.map((productItem) => (
             <p key={productItem.id}>{productItem.title}</p>
