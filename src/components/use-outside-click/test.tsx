01e72fb6f4a1eff0f1fe18ccd69dbd2b1b931105
vtouzard <vtouzard244@gmail.com>
2024-03-05 09:26:21 +0100

    feat: Ajout de la logique d'appelle de mon hook

--- src/components/use-outside-click/test.tsx
+++ src/components/use-outside-click/test.tsx
@@ -1,12 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import UseOutsideClick from ".";
 
 const UseOnClickOutsideTest = () => {
+  const ref = useRef<any>();
+  UseOutsideClick(ref, () => setShowContent(false))
   const [showContent, setShowContent] = useState(false);
+
+  useEffect(() =>{
+
+  })
 
   return (
     <div>
       {showContent ? (
-        <div>
+        <div ref={ref}>
           <h1>This is a random content</h1>
           <p>
             Please click outside of this to close this. it won't close if you
