5b4b7cacf6f3c3301bb2f62576ac902d8ee9ab82
vtouzard <vtouzard244@gmail.com>
2024-03-05 09:18:15 +0100

    feat: Ajout de la structure du hook custom onClick

--- src/components/use-outside-click/index.tsx
+++ src/components/use-outside-click/index.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect } from "react";
 
-const UseOutsideClick = () => {
-  return (
-    <div>UseOutsideClick</div>
-  )
-}
+const UseOutsideClick = (ref: any, handler: Function) => {
+  useEffect(() => {
+    const listener = (e: any) => {
+      if (!ref.current || ref.current.contains(e.target)) {
+        return;
+      }
 
-export default UseOutsideClick
+      handler(e);
+    };
+
+    document.addEventListener("mousedown", listener);
+    document.addEventListener("touchstart", listener);
+
+    return () =>{
+        document.removeEventListener("mousedown", listener);
+        document.removeEventListener("touchstart", listener);
+    }
+  }, [handler, ref]);
+};
+
+export default UseOutsideClick;
