583f020d63138bc81aea0db7d8ba43a55514f0cb
vtouzard <vtouzard244@gmail.com>
2024-03-06 09:48:43 +0100

    feat: Ajout de la logique pour notre hook personnalisé

--- src/components/use-window-resize/index.tsx
+++ src/components/use-window-resize/index.tsx
@@ -1,6 +1,34 @@
-import React from 'react'
+import React, { useLayoutEffect, useState } from "react";
 
 const UseWindowResize = () => {
-}
+  const [windowSize, setWindowSize] = useState({
+    with: 0,
+    height: 0,
+  });
 
-export default UseWindowResize
+  /**
+   * A la différence du use Effect, le useLayoutEffect effectue la logique avant que les éléments du DOM ne soit affichés
+   */
+  useLayoutEffect(() => {
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  /**
+   * Permet de stocker les changements de taille d'écrans dans le state
+   */
+  const handleResize = () => {
+    setWindowSize({
+      with: window.innerWidth,
+      height: window.innerHeight,
+    });
+  };
+
+  return windowSize
+};
+
+export default UseWindowResize;
