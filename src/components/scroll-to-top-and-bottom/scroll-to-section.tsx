9a2a6e1a56d2992e0d74ca2feb506b90f242f4be
vtouzard <vtouzard244@gmail.com>
2024-03-06 17:52:02 +0100

    feat: Ajout de la logique pour le scrollToSection

--- src/components/scroll-to-top-and-bottom/scroll-to-section.tsx
+++ src/components/scroll-to-top-and-bottom/scroll-to-section.tsx
@@ -1,57 +1,69 @@
-import React from "react";
+import React, { useRef } from "react";
 
 const ScrollToSection = () => {
-    const data = [
-        {
-            label: 'First Card',
-            style:{
-                width: '100%',
-                height: '600px',
-                background: 'red'
-            }
-        },
-        {
-            label: 'Second Card',
-            style:{
-                width: '100%',
-                height: '600px',
-                background: 'gray'
-            }
-        },
-        {
-            label: 'Third Card',
-            style:{
-                width: '100%',
-                height: '600px',
-                background: 'blue'
-            }
-        },
-        {
-            label: 'Fourth Card',
-            style:{
-                width: '100%',
-                height: '600px',
-                background: 'green'
-            }
-        },
-        {
-            label: 'Fifth Card',
-            style:{
-                width: '100%',
-                height: '600px',
-                background: 'black'
-            }
-        },
-    ]
+  const ref = useRef<HTMLDivElement>(null);
+
+  const data = [
+    {
+      label: "First Card",
+      style: {
+        width: "100%",
+        height: "600px",
+        background: "red",
+      },
+    },
+    {
+      label: "Second Card",
+      style: {
+        width: "100%",
+        height: "600px",
+        background: "gray",
+      },
+    },
+    {
+      label: "Third Card",
+      style: {
+        width: "100%",
+        height: "600px",
+        background: "blue",
+      },
+    },
+    {
+      label: "Fourth Card",
+      style: {
+        width: "100%",
+        height: "600px",
+        background: "green",
+      },
+    },
+    {
+      label: "Fifth Card",
+      style: {
+        width: "100%",
+        height: "600px",
+        background: "black",
+      },
+    },
+  ];
+
+  const handleScrollToSection = () =>{
+    let pos = ref.current?.getBoundingClientRect().top;
+
+    window.scrollTo({
+      top: pos,
+      behavior: "smooth",
+    })
+  }
+
   return (
     <div>
       <h1>Scroll to a particular section</h1>
-      <button>Click to scroll</button>
-      {
-        data.map((item, index) => <div key={index}>
-            <h3 style={item.style}>{item.label}</h3>
-        </div>)
-      }
+      <button onClick={handleScrollToSection}>Click to scroll</button>
+      {data.map((item, index) => (
+        <div ref={index === 3 ? ref : null} key={index}>
+          <h3 style={item.style}>{item.label}</h3>
+        </div>
+      ))}
     </div>
   );
 };
