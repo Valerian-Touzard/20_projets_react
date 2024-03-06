e549e70106e95ed1183ebfcb8f620d256b38f4d7
vtouzard <vtouzard244@gmail.com>
2024-03-06 10:09:51 +0100

    feat: Ajout de la possibilité de remonter en haut de la page

--- src/components/scroll-to-top-and-bottom/index.tsx
+++ src/components/scroll-to-top-and-bottom/index.tsx
@@ -14,23 +14,41 @@ const ScrollToTopAndBottom = () => {
     {}
   );
 
-  if (error){
-    return <h1>Error occured ! Please try again</h1>
+  /**
+   * Permet de "remonter" la page en haut du site de manière douce
+   */
+  const handleScrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  };
+
+  /**
+   * Permet de "descendre" la page en bas du site de manière douce
+   */
+  const handleScrollToBottom = () => {};
+
+  if (error) {
+    return <h1>Error occured ! Please try again</h1>;
   }
-  if (pending){
-    return <h1>Loading ! Please wait</h1>
+  if (pending) {
+    return <h1>Loading ! Please wait</h1>;
   }
   return (
     <div>
       <h1>Scroll To Top And Bottom feature</h1>
       <h3>This is the top section</h3>
       <button>Scroll To Bottom</button>
-      <ul style={{listStyle: 'none'}}>
+      <ul style={{ listStyle: "none" }}>
         {data && data.products.length
-          ? data.products.map((product) => <li key={product.id}>{product.title}</li>)
+          ? data.products.map((product) => (
+              <li key={product.id}>{product.title}</li>
+            ))
           : null}
       </ul>
-      <button>Scroll To Top</button>
+      <button onClick={handleScrollToTop}>Scroll To Top</button>
       <h3>This is the bottom of the page</h3>
     </div>
   );
