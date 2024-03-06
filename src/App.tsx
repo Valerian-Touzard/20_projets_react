5470c4e3869bb2f9737498bc3fe560b8a80cd7df
vtouzard <vtouzard244@gmail.com>
2024-03-06 10:25:03 +0100

    feat: Affichage du composant

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,7 @@ import UseFetchHookTest from './components/use-fetch/test';
 import UseOnClickOutsideTest from './components/use-outside-click/test';
 import UseWindowResizeTest from './components/use-window-resize/test';
 import ScrollToTopAndBottom from './components/scroll-to-top-and-bottom';
+import ScrollToSection from './components/scroll-to-top-and-bottom/scroll-to-section';
 
 function App() {
   return (
@@ -82,7 +83,10 @@ function App() {
       {/* <UseWindowResizeTest /> */}
 
       {/* Scroll to Top and Bottom */}
-      <ScrollToTopAndBottom />
+      {/* <ScrollToTopAndBottom /> */}
+
+      {/* Scroll to Section */}
+      <ScrollToSection />
     </div>
   );
 }
